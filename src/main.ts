a4a099fd9901e78f83280bc6dab4a4532026f0ac
Benjamim Rees <benjamimrees@gmail.com>
2025-10-31 01:50:23 -0300

    fix: docker compose

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,6 @@ async function bootstrap() {
             persistAuthorization: true,
         },
     });
-    await app.listen(process.env.PORT ?? 3000);
+    await app.listen(80);
 }
 bootstrap();
